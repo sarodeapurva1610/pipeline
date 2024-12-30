393067f1218ac5c688075bc2d46ae98b3c0e33d5
sarodeapurva1610 <apurvasarode1610@gmail.com>
2024-12-30 16:49:12 +0530

    pipeline.java

--- pipeline.java
+++ pipeline.java
@@ -2,6 +2,6 @@ public class pipeline
 {
   public static void main(String args[])
   {
-    System.out.println("Welcome to the CI/CD Pipeline Practical!")
+    System.out.println("Welcome to the CI/CD Pipeline Practical!");
   }
 }
